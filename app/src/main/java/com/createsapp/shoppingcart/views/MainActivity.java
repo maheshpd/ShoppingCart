2f654682cfc3c285685369380409bdadbdce936e
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-02-22 21:23:17 +0530

    6. Shopping Cart (MVVM) - Navigation Menu & AppBar Back-button

--- app/src/main/java/com/createsapp/shoppingcart/views/MainActivity.java
+++ app/src/main/java/com/createsapp/shoppingcart/views/MainActivity.java
@@ -1,11 +1,14 @@
 package com.createsapp.shoppingcart.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.createsapp.shoppingcart.R;
 
@@ -19,7 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this,navController);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController) || super
+                .onOptionsItemSelected(item);
+
     }
 }
