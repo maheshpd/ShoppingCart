82152cf8451237d0b40c200084e44ff769988376
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-02-22 19:49:16 +0530

    Shopping Cart (MVVM) - Product Model & Shop Recycler view

--- app/src/main/java/com/createsapp/shoppingcart/views/ShopFragment.java
+++ app/src/main/java/com/createsapp/shoppingcart/views/ShopFragment.java
@@ -2,6 +2,8 @@ package com.createsapp.shoppingcart.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,8 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.createsapp.shoppingcart.R;
+import com.createsapp.shoppingcart.adapters.ShopListAdapter;
+import com.createsapp.shoppingcart.databinding.FragmentShopBinding;
+import com.createsapp.shoppingcart.models.Product;
 
-public class ShopFragment extends Fragment {
+public class ShopFragment extends Fragment implements ShopListAdapter.ShopInterface{
+
+
+    FragmentShopBinding fragmentShopBinding;
+    private ShopListAdapter shopListAdapter;
 
     public ShopFragment() {
         // Required empty public constructor
@@ -20,6 +29,25 @@ public class ShopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        fragmentShopBinding = FragmentShopBinding.inflate(inflater, container, false);
+        return fragmentShopBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        shopListAdapter = new ShopListAdapter();
+
+        fragmentShopBinding.shopRecyclerView.setAdapter(shopListAdapter);
+    }
+
+    @Override
+    public void addItem(Product product) {
+
+    }
+
+    @Override
+    public void onItemClick(Product product) {
+
     }
 }
